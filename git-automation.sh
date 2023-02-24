7a44b4a9b80f38d9c0179d70befd97a4b139f3b2
avnish kumar <11avnishkumar@gmail.com>
2023-02-24 11:05:09 +0530

    main

---
+++ git-automation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get user input for repository name and branch name
+#read -p "Enter the name of the repository you want to update: " repo_name
+#read -p "Enter the name of the branch you want to update: " branch_name
+echo "Welcome to Git automation" 
+git status
+read -p "Enter the name of the branch you want to update: " message
+git add -A
+git commit -m "$message"
+git push
+
+echo "Repository updated successfully!"
+
