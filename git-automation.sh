8afdeaa45a28cf492992eafb70145b8ecfb79f78
avnish kumar <11avnishkumar@gmail.com>
2023-02-24 11:06:07 +0530

    updated

--- git-automation.sh
+++ git-automation.sh
@@ -5,7 +5,7 @@
 #read -p "Enter the name of the branch you want to update: " branch_name
 echo "Welcome to Git automation" 
 git status
-read -p "Enter the name of the branch you want to update: " message
+read -p "Enter the commit message: " message
 git add -A
 git commit -m "$message"
 git push
